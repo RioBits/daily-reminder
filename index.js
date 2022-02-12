70c71f0d1ec7584426cc779ef8f83666605df8fd
riobits <23579928+RioBits@users.noreply.github.com>
2022-02-12 22:29:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+})
+
+const mailOptions = {
+  from: process.env.USER,
+  to: process.env.EMAIL_RECEIEVER,
+  subject: "Dont't forget to code!",
+  text: "Yo bro don't forget to write some code! ;) ",
+}
+
+transporter.verify((err) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Server is ready to take our messages')
+  }
+})
+
+const start = async () => {
+  while (true) {
+    const now = new Date()
+    let millisTill10 =
+      new Date(now.getFullYear(), now.getMonth(), now.getDate(), 22, 0, 0, 0) -
+      now
+
+    if (millisTill10 < 0) {
+      millisTill10 += 86400000 // it's after 10pm, try 10pm tomorrow.
+    }
+
+    const newReminder = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          transporter.sendMail(mailOptions, (err) => {
+            if (err) {
+              console.error(err)
+            } else {
+              console.log(
+                'Email sent successfully!',
+                new Date().toLocaleDateString('en-US')
+              )
+            }
+          })
+          resolve()
+        }, millisTill10)
+      })
+    }
+    await newReminder()
+  }
+}
+
+start()
